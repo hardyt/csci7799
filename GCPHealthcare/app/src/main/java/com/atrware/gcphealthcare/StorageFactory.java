ce5715e7c3049ef3d21a23f442efabf7ff9fc0ed
hardyt <odessa131@aol.com>
2016-04-19 11:12:58 -0600

    GCS download working.

---
+++ GCPHealthcare/app/src/main/java/com/atrware/gcphealthcare/StorageFactory.java
@@ -0,0 +1,7 @@
+package com.atrware.gcphealthcare;
+
+/**
+ * Created by Tom on 4/19/2016.
+ */
+public class StorageFactory {
+}
---
+++ GCPHealthcare/app/src/main/java/com/atrware/gcphealthcare/gcs.java
@@ -0,0 +1,7 @@
+package com.atrware.gcphealthcare;
+
+/**
+ * Created by Tom on 4/18/2016.
+ */
+public class gcs {
+}
